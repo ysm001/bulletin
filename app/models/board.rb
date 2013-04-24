cbeee79def2d76b21cf9af5db115d58515286639
ry ysd <ry.ysd01@gmail.com>
2013-04-25 00:13:02 +0900

    comment implementation

--- app/models/board.rb
+++ app/models/board.rb
@@ -2,5 +2,8 @@ class Board
   include Mongoid::Document
   field :title, type: String
 
+  validates_presence_of :title
+  validates_uniqueness_of :title
+
   has_many :comments
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,9 @@
 class Comment
   include Mongoid::Document
+  include Mongoid::Timestamps
   field :content, type: String
+
+  validates_presence_of :content
 
   belongs_to :board
 end
